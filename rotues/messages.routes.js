03857854cbe24deb85fbf76b81013eec383f5a2e
Parado-xy <jallaamaju@gmail.com>
2025-07-02 13:10:06 -0500

    updated the page

--- rotues/messages.routes.js
+++ rotues/messages.routes.js
@@ -1,39 +1,49 @@
+// Import the router object from express. 
 import { Router } from "express";
+
+// Import axios for sending request. 
 import axios from "axios";
 
-const messageRouter = Router(); 
+// Instantiate router object; 
+const messageRouter = new Router(); 
 
-messageRouter.post(`/new-message`, async (req, res, next) => {
-    const { message } = req.body;
-
-    //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-    if (!message ) {
-      // In case a message is not present,  do nothing and return an empty response
-      return res.end();
-    }
-
-    // If we've gotten this far, it means that we have received a message containing the word "marco".
-    // Respond by hitting the telegram bot API and responding to the appropriate chat_id with the word "Polo!!"
-    // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-    axios
-      .post(
-        "https://api.telegram.org/bot777845702:AAFdPS_taJ3pTecEFv2jXkmbQfeOqVZGER/sendMessage",
-        {
-          chat_id: message.chat.id,
-          text: "Polo!!",
-        }
-      )
-      .then((response) => {
-        // We get here if the message was successfully posted
-        console.log("Message posted");
-        res.end("ok");
-      })
-      .catch((err) => {
-        // ...and here if it was not
-        console.log("Error :", err);
-        res.end("Error :" + err);
-      });
-}); 
-
+// Export router object; 
 export default messageRouter; 
+
+// attach a message route. 
+messageRouter.post(`/new-message`, async (req, res, next) => {
+    try{
+        const { message } = req.body;
+
+        // Let's actually log the request body
+        console.log(req.body); 
+
+
+        //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+
+        if (!message) {
+          return res.end();
+        }
+
+        console.log(`message: ${message}`);  
+
+        const BOT_TOKEN = process.env.BOT_TOKEN || "7828660251:AAGAUoji46BxC4pg3yJeb2AAc2HRLmGlYUM";
+
+        axios
+          .post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+            chat_id: message.chat.id,
+            text: "Welcome to the WatchJallaCode Telegram channel!",
+          })
+          .then((response) => {
+            // We get here if the message was successfully posted
+            console.log("Message posted");
+            res.end("ok");
+          })
+          .catch((error) => {
+            console.error("Failed to send message:", error);
+            next(error);
+          });
+    }catch(error){
+        next(error); 
+    }
+})
